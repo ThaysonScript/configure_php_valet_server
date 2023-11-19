1c8dd8a3fdea10b3676b2497cfe022aeee5146e8
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-11-19 13:37:36 -0300

    Create configure_paths.sh

---
+++ configure_paths.sh
@@ -0,0 +1,10 @@
+# adicionar caminho ao valet path
+valet park
+
+# verificar o caminho adicionado
+valet paths
+
+# crie um novo codigo php dentro do caminho dito no valet paths
+
+# logo apos digite a pasta que dentro tem um arquivo php no navegador
+# exemplo: pasta_php.test
